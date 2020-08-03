d9690faa6b352d89895b8dfde3ae69f0018cf7c3
parupati <madhukar.parupati@gmail.com>
2020-08-03 16:20:00 +0530

    Add files via upload

---
+++ {{cookiecutter.directory_name}}/src/env.js
@@ -0,0 +1,11 @@
+(function (window) {
+    window.__env = window.__env || {};
+
+    window.__env.apiUrl = 'abc';
+    window.__env.adfsURL = 'abc';
+    window.__env.client_id = 'abc';
+    window.__env.redirect_uri = 'abc';
+    window.__env.aws_region = 'abc';
+    window.__env.provider_url = 'abc';
+    window.__env.pool_id = 'abc';
+}(this));
